b8ee09979792f1f34c10788a9d4fa27298e1bed2
Dan Christian Febra <danchristianfebra@Dans-iMac-Pro.local>
2025-08-13 23:57:06 +0800

    added asynchronous connection for the postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, TweetModule, AuthModule],
+  imports: [UsersModule, TweetModule, AuthModule, TypeOrmModule.forRootAsync({
+    imports: [],
+    inject: [],
+    useFactory: () => ({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'danfebra',
+      database: 'nestjs',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
+      logging: true
+    })
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
