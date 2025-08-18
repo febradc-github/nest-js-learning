8f9ead51dc1eb8d07bd984469c08c3b99b8824ef
Dan Christian Febra <febra.dc+github@gmail.com>
2025-08-18 17:30:58 +0800

    updated a proper logging information about the application running

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   await app.listen(port);
   const logger = new Logger('Bootstrap');
-  logger.log(`Application is running on: http://localhost:${port}`);
+  logger.log(`Application is currently running on: http://localhost:${port}`);
 }
 bootstrap();
